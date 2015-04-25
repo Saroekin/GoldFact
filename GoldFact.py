5c141a4cfc38da62eb226482aed81961476648d8
Connor Burt <connor.b@outlook.com>
2015-04-25 00:21:34 -0500

    Updated copy of GoldFact.py

--- GoldFact.py
+++ GoldFact.py
@@ -186,7 +186,7 @@ def run_bot_comments_lounge():
         try:
             comment_text = comment.body.lower()
             if str(comment.author) != Username:
-                commentNum = randint(0,9)
+                commentNum = randint(0,13)
                 if commentNum == 2:
                     comment.reply(commentsubmit)
         except AttributeError:
@@ -213,7 +213,7 @@ def run_bot_comments_all():
         try:
             comment_text = comment.body.lower()
             if str(comment.author) != Username:
-                commentNum = randint(0,19)
+                commentNum = randint(0,32)
                 if commentNum == 2:
                     comment.reply(commentsubmit)
         except AttributeError:
